03be87a1baf25b4c1519ebb0f5b7445bafb89240
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-06-17 17:26:20 +0200

    switch to pathlib, improve cli args

--- tree_stat/__main__.py
+++ tree_stat/__main__.py
@@ -2,6 +2,7 @@
 import argparse
 import logging
 import os
+from pathlib import Path
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -11,8 +12,7 @@ from tree_stat._display_file_size import COMMERCIAL, INFORMATICS, display_file_s
 log = logging.getLogger(__name__)
 
 
-def tree_stat(directory=None):
-    directory = directory or os.getcwd()
+def tree_stat(directory):
     directory_measures = take_measures(directory)
 
     env = Environment(
@@ -27,8 +27,10 @@ def tree_stat(directory=None):
 
     report = template.render(directory_measures=reversed(list(directory_measures.values())))
 
-    with open('tree_stat.md', 'w') as f:
-        f.write(report)
+    if args.print or not args.output:
+        print(report)
+    if args.output:
+        args.output.write_text(report)
 
 
 def take_measures(directory):
@@ -58,18 +60,19 @@ def take_measures(directory):
 def main():
     global args
     parser = argparse.ArgumentParser(description='Find files recursively and compute size of each directory level')
-    parser.add_argument('directory', nargs='?', help='a directory to search in')
+    parser.add_argument('directory', type=Path, nargs='?', default=Path.cwd(), help='a directory to search in')
     # parser.add_argument('-e', '--extensions', nargs='+', metavar='extension', required=False,
     #                     help='only count files with these extensions')
-    parser.add_argument('-c', '--classify', default=False, action='store_true',
-                        help='Display size by file type')
+    parser.add_argument('-o', '--output', type=Path, help='File to write to result into')
+    parser.add_argument('--print', default=False, action='store_true',
+                        help='Print result to standard output, it is the default if --output is not specified')
     parser.add_argument('--commercial-base', dest='coefficient_base', default=INFORMATICS, action='store_const',
                         const=COMMERCIAL, help='By default, size is printed using coefficient with base 1024.'
                                                ' This option sets coefficient base to 1000')
-    parser.add_argument('--DEBUG', default=False, action='store_true', help='Display debug logs')
+    parser.add_argument('--verbose', default=False, action='store_true', help='Display debug logs')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG if args.DEBUG else logging.INFO)
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
     tree_stat(args.directory)
 
