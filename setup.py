7d211e4fec8d134ee4874889c5efc0d2210c7bc2
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-06-19 16:32:33 +0200

    document compatibility with python versions

--- setup.py
+++ setup.py
@@ -21,6 +21,10 @@ setuptools.setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Alpha'
     ],
