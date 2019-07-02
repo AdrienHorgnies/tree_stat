cf3c604be9f9038c50749eb7964c3b2b004cf1d1
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2019-07-02 18:57:16 +0200

    save output to tmp directory if access is denied

--- tree_stat/__main__.py
+++ tree_stat/__main__.py
@@ -2,6 +2,7 @@
 import argparse
 import logging
 import os
+from tempfile import mkdtemp
 from pathlib import Path
 
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -12,7 +13,7 @@ from tree_stat._formatter import COMMERCIAL, INFORMATICS, format_file_size, path
 log = logging.getLogger(__name__)
 
 
-def tree_stat(directory, pov, light):
+def tree_stat(directory, must_print=None, output=None, light=False, pov=None):
     directory_measures = take_measures(directory)
 
     env = Environment(
@@ -31,10 +32,16 @@ def tree_stat(directory, pov, light):
 
     report = template.render(directory_measures=directory_measures)
 
-    if args.print or not args.output:
+    if must_print or not output:
         print(report)
-    if args.output:
-        args.output.write_text(report)
+    if output:
+        try:
+            output.write_text(report)
+        except PermissionError:
+            tmp_dir = mkdtemp(prefix='tree_stat_')
+            replacement_path = Path(tmp_dir, output.name)
+            replacement_path.write_text(report)
+            log.error('Permission to write to {} was denied, wrote to {} instead'.format(output, replacement_path))
 
 
 def take_measures(directory):
@@ -67,7 +74,7 @@ def main():
     parser.add_argument('directory', type=Path, nargs='?', default=Path.cwd().relative_to(Path.cwd()),
                         help='a directory to search in')
     parser.add_argument('-o', '--output', type=Path, help='File to write to result into')
-    parser.add_argument('--print', default=False, action='store_true',
+    parser.add_argument('--print', dest='must_print', default=False, action='store_true',
                         help='Print result to standard output, it is the default if --output is not specified')
     parser.add_argument('--commercial-base', dest='coefficient_base', default=INFORMATICS, action='store_const',
                         const=COMMERCIAL, help='By default, size is printed using coefficient with base 1024.'
@@ -81,7 +88,7 @@ def main():
 
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-    tree_stat(args.directory, args.pov, args.light)
+    tree_stat(args.directory, must_print=args.must_print, output=args.output, light=args.light, pov=args.pov)
 
 
 if __name__ == '__main__':
